58e0eb1995e9ddcfd979988e3201ec932d9570da
vishal acharya <vishalacharya814@gmail.com>
2023-03-29 14:11:54 +0530

    changes

--- src/components/call-to-action.tsx
+++ src/components/call-to-action.tsx
@@ -84,9 +84,9 @@ export const CallToAction = () => {
         />
       </div> */}
       <div className="mt-6 m-auto space-y-6 md:w-8/12 lg:w-7/12">
-        <h3 className="text-center text-2xl font-bold text-gray-800 dark:text-white md:text-4xl">Become a greenbowl insider</h3>
+        <h3 className="text-center text-2xl font-bold text-gray-800 dark:text-white md:text-4xl">Launching soon !</h3>
      
-        <div className="flex flex-wrap justify-center gap-6">
+        {/* <div className="flex flex-wrap justify-center gap-6">
           <input 
           value={email}
           onChange={(e) => setEmail(e.target.value)}
@@ -99,7 +99,7 @@ export const CallToAction = () => {
                 >{btn}</span
               >
             </a>
-           {/* <a
+           <a
               href="#"
               className="relative flex h-12 w-full items-center justify-center px-8 before:absolute before:inset-0 before:rounded-full before:border before:border-transparent before:bg-primary/10 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-gray-800 sm:w-max"
             >
@@ -107,8 +107,8 @@ export const CallToAction = () => {
                 className="relative text-base font-semibold text-primary dark:text-white"
                 >More about</span
               >
-          </a> */}
-        </div>
+          </a>
+        </div> */}
       </div>
     </div>
   </Container>
